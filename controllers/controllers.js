f0aad40e28187961be849577f64c7e2a04559599
usamabilal <khawajausama775@gmail.com>
2021-01-22 21:34:46 +0500

    Update 1

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -7,7 +7,7 @@ exports.checkPlayer = (req, res) => {
     const playerId = req.query.id;
 
     //search for playerid
-    let sql = "Select * from player_details where playerId='" + playerId + "'";
+    let sql = "Select * from (player_details inner join purchaseditems on player_details.playerId=purchaseditems.playerId) where player_details.playerId='" + playerId + "'";
 
     const token = jwt.sign({
         playerId: playerId
@@ -30,56 +30,174 @@ exports.addPlayer = (req, res) => {
 
     let playerId = req.body.playerId;
     let username = req.body.username;
+    let numberFlag = false;
+    let lengthFlag = false;
 
     //Username Pattern Validation
-    // var patt = new RegExp("/^[A-Za-z0-9]{8,9}$/");
-    // var flag = patt.test(username);
-    // console.log(flag);
+    if (username.length == 8) {
+        lengthFlag = true;
+    }
 
-    let sql = "INSERT INTO player_details (playerId,username) VALUES ('" + playerId + "','" + username + "')";
+    for (let i = 0; i < username.length; i++) {
+        if (username[i] >= '0' && username[i] <= "9") {
+            numberFlag = true;
+        }
+    }
 
-    con.query(sql, (err, result) => {
-        if (err) {
-            return res.status(500).json({ message: err.message });
-        };
-        //create a token
-        const token = jwt.sign({
-            playerId: playerId
-        }, "kwi9owl");
-        
-        let sql = "INSERT INTO purchasedItems (playerId) VALUES ('" + playerId + "')";
+    var patt = new RegExp("^[a-zA-Z0-9]*$");
+    var flag = patt.test(username);
+    if (flag & lengthFlag & numberFlag) {
+
+        let sql = "INSERT INTO player_details (playerId,username) VALUES ('" + playerId + "','" + username + "')";
+
         con.query(sql, (err, result) => {
             if (err) {
-               
-           }
-        });
+                return res.status(500).json({ message: err.message });
+            };
+            //create a token
+            const token = jwt.sign({
+                playerId: playerId
+            }, "kwi9owl");
 
-        return res.status(200).json({ message: "Player Added", token: token });
-    });
+            let sql = "INSERT INTO purchasedItems (playerId) VALUES ('" + playerId + "')";
+            con.query(sql, (err, result) => {
+                if (err) {
+                    return res.status(500).json({ message: err.message });
+                }
+                let sql = "INSERT INTO friendRequests (playerId) VALUES ('" + playerId + "')";
+                con.query(sql, (err, result) => {
+                    if (err) {
+                        return res.status(500).json({ message: err.message });
+                    }
+                    let sql = "INSERT INTO friends (playerId) VALUES ('" + playerId + "')";
+                    con.query(sql, (err, result) => {
+                        if (err) {
+                            return res.status(500).json({ message: err.message });
+                        }
+                        return res.status(200).json({ message: "Player Added", token: token });
+                    });
+                });
+            });
+
+        });
+    }
+    else {
+        return res.status(500).json({ message: "Username should contain be of length 8 characters including number" });
+    }
+
 }
 
 exports.sendRequest = (req, res) => {
 
-    let senderId = req.playerId;
-    let recId = req.body.recId;
+    let toSend = req.body.toSend;
 
-    let sql = "Select * from friendrequests where fr_sent=' " + senderId + "' AND fr_rec='" + recId + "'";
+    let sql = "Select * from friendrequests where playerId='" + req.playerId + "'";
     db.query(sql, (err, result) => {
-        if (err) throw err;
-        if (result.length > 0) {
-            return res.status(200).json({ message: "Request Already Sent" });
+        if (err) {
+            return res.status(500).json({ message: err.message });
         }
         else {
-            let sql = "INSERT INTO friendrequests (fr_sent,fr_rec) VALUES ('" + senderId + "','" + recId + "')";
-            db.query(sql, (err, result) => {
-                if (err) throw err;
-                return res.status(200).json({ message: "Request Sent Successfully" });
-            })
+            let fr_sent;
+            let flag1 = false;
+            let flag2 = false;
+            let fr_sent_new;
+            let fr_recieved;
+
+            if (result[0].fr_sent) {
+                fr_sent = result[0].fr_sent.split(',');
+                fr_sent_new = result[0].fr_sent + "," + toSend;
+                fr_sent.map(fr => {
+                    if (fr == toSend) {
+                        flag1 = true;
+                        return res.status(200).json({ message: "Request Sent Already" });
+                    }
+                });
+            }
+            else {
+                fr_sent_new = toSend;
+            }
+
+            if (result[0].fr_recieved) {
+                fr_recieved = result[0].fr_recieved.split(',');
+                fr_recieved.map(fr => {
+                    if (fr == toSend & flag1 == false) {
+                        flag2 = true;
+                        return res.status(200).json({ message: "You already got the request of same person" });
+                    }
+                });
+            }
+
+            if (flag1 == false & flag2 == false) {
+                let sql = "Update friendrequests set fr_sent='" + fr_sent_new + "'" + " where playerId='" + req.playerId + "'";
+                db.query(sql, (err, result) => {
+                    if (err) return res.status(500).json({ message: err.message });
+                    if (result) {
+                        return res.status(200).json({ message: "Request Sent Successfully" });
+                    }
+                });
+            }
         }
-    })
+    });
 
 }
 
-exports.receiveReq = (req, res) => {
+
+exports.acceptReq = (req, res) => {
+
+    let acceptedId = req.body.acceptedId;
+    let playerId = req.playerId;
+
+    console.log(acceptedId,playerId);
+
+    let sql = "Select * from friends where playerId='" + playerId + "'";
+    db.query(sql, (err, result) => {
+        if (err) {
+            return res.status(500).json({ message: err.message });
+        }
+
+        let newFriendList;
+        if (result[0].friends) {
+            newFriendList = result[0].friends + "," + acceptedId;
+        }
+        else {
+            newFriendList = acceptedId;
+        }
+
+        let sql = "Update friends set friends='" + newFriendList + "'" + " where playerId='" + req.playerId + "'";
+        con.query(sql, (err, result) => {
+
+            if (err) {
+                return res.status(500).json({ message: err.message });
+            }
+
+            let sql = "Select * from friendrequests where playerId='" + req.playerId + "'";
+            db.query(sql, (err, result) => {
+                if (err) {
+                    return res.status(500).json({ message: err.message });
+                }
+
+                let friendReqs = (result[0].fr_recieved).split(',');
+                let newFrs;
+
+                friendReqs.map(fr => {
+                    if (fr !== acceptedId) {
+                        newFrs = "," + fr;
+                    }
+                })
+
+                let sql = "Update friendrequests set fr_recieved='" + newFrs + "'" + " where playerId='" + req.playerId + "'";
+                db.query(sql, (err, result) => {
+                    if (err) return res.status(500).json({ message: err.message });
+                    if (result) {
+                        return res.status(200).json({ message: "Friend Added Successfully" });
+                    }
+                });
+
+            })
+
+        })
+
+    })
+
 
 }
--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,7 @@ const isAuth = require("./../controllers/auth");
 router.get("/checkPlayer?:id", controllers.checkPlayer);
 router.post("/addPlayer", controllers.addPlayer);
 router.post("/sendFriendRequest", isAuth, controllers.sendRequest);
+router.post("/acceptRequest", isAuth, controllers.acceptReq);
 
 
 module.exports = router;
